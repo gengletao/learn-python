62b756f8184ed774fe0911e856994afee8c243ba
letao <gengletao@gmail.com>
2011-10-09 00:32:33 +0800

    some tests

--- gothonweb/bin/app.py
+++ gothonweb/bin/app.py
@@ -10,12 +10,12 @@ import web
 
 urls = (
         '/hello', 'Index',
-        '/(.*)', 'hello'
+        #/(.+)', 'hello',
         )
 
 app = web.application(urls, globals())
 
-render = web.template.render('templates/')
+render = web.template.render('templates/', base="layout")
 
 class Index(object):
     def GET(self):
